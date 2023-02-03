df02a9b83ac4fa7025eb8feb6ffbf9ed319752be
dclxviclan <109929818+dclxviclan@users.noreply.github.com>
2023-02-03 15:09:39 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 from tensorflow import keras
 import matplotlib.pyplot as plt
 import numpy as np
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-plt.imshow(x_train[0], cmap='Greys')
+plt.imshow(x_train[1], cmap='Greys')
