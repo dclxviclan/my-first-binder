8631daefce6031c4dc8b969a42c4efe7b86d6363
dclxviclan <109929818+dclxviclan@users.noreply.github.com>
2023-02-03 11:52:57 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello from binder")
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import numpy as np
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+plt.imshow(x_train[0], cmap='Greys')
